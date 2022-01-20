39745b54359a6ca10de833f4c93444acef482ba3
rohitshahane07 <shahanerohit200@gmail.com>
2022-01-20 12:29:31 +0530

    hlo

---
+++ script.js
@@ -0,0 +1,46 @@
+const quizeData = [
+    {
+        question:'How old is MS.dhoni?',
+        a: '24',
+        b: '45',
+        c: '30',
+        d: '40',
+        correct: 'd',
+    } ,{
+        question: 'Who is best Captain? ',
+        a: 'MS.DHONI',
+        b: 'Rohit Sharma',
+        c: 'Hardik Pandy',
+        d: 'KL. Rahul',
+        correct: 'a',
+    }, {
+        question:'What is the most used Prgramming language in 2022?',
+        a: 'java',
+        b: 'Paython',
+        c: 'c',
+        d: 'Java Script',
+        correct: 'b'
+          
+    }, {
+        question: 'Who is the Pricidant of Indai',
+        a: 'Rohit Shahane ',
+        b: 'Abhi kambale',
+        c: 'Narendra Modi',
+        d: 'Ajay andhaare',
+        correct:'c'
+    }, {
+        question:'Who is Royan man',
+        a: 'Rohit Shahane',
+        b: 'Abhi kambale',
+        c: 'Ajay Andhare',
+        d: 'All of the above',
+        correct:'d'
+    },{
+        question: 'HTML stand form',
+        a: 'Hypertext markup language',
+        b: 'Hyper language',
+        c: 'Hyper markup language',
+        d: 'non of the above',
+    }
+    
+]
