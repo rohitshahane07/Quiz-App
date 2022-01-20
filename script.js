85545e60eaa6fd7f03e6884fc4746715f1908603
rohitshahane07 <shahanerohit200@gmail.com>
2022-01-20 21:10:49 +0530

    quze app

--- script.js
+++ script.js
@@ -44,3 +44,37 @@ const quizeData = [
     }
     
 ]
+const questionEl =document.getElementById('question')
+const a_text =document.getElementById('a_text')
+const b_text =document.getElementById('b_text')
+const c_text =document.getElementById('c_text')
+const d_text =document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz =0;
+
+loadQuiz()
+
+function loadQuiz() {
+    const currentQuizData =quizeData[currentQuiz];
+    questionEl.innerHTML = currentQuizData.question
+
+    a_text.innerHTML =currentQuizData.a;
+    b_text.innerHTML =currentQuizData.b;
+    c_text.innerHTML =currentQuizData.c;
+    d_text.innerHTML =currentQuizData.d;
+    
+  
+
+}
+submitBtn.addEventListener('click', () =>{
+    currentQuiz++;
+    if(currentQuiz<quizeData.length){
+        loadQuiz();
+
+    }
+    else (
+        alert('You fineshed')
+    )
+
+})
